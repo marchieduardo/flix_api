2d860f8d98d4253aa8f7e3c98bdd103cc8924f95
Eduardo Marchi <edu.3e@hotmail.com>
2025-01-24 22:38:39 -0300

    Adicionado endpoint que, ao receber POST, cria novo cadastro de gênero no banco com o nome recebido no body do JSON.

--- genres/views.py
+++ genres/views.py
@@ -1,9 +1,22 @@
+import json
 from django.shortcuts import render
 from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 from genres.models import Genre
 
 
+@csrf_exempt     # -----> Decorator de excessão do csrf token, necessário para autorizar o POST
 def genre_view(request):
-    genres = Genre.objects.all()
-    data = [{'id': genre.id, 'name': genre.name} for genre in genres]
-    return JsonResponse(data, safe=False)
+    if request.method == 'GET':     # -----> Quando é GET, retorna do banco a lista de gêneros cadastrados
+        genres = Genre.objects.all()
+        data = [{'id': genre.id, 'name': genre.name} for genre in genres]
+        return JsonResponse(data, safe=False)
+    
+    elif request.method == 'POST':     # -----> Quando é POST, captura os dados do body da request, e armazena no banco de dados
+        data = json.loads(request.body.decode('utf-8'))
+        new_genre = Genre(name=data['name'])
+        new_genre.save()
+        return JsonResponse(
+            {'id': new_genre.id, 'name': new_genre.name},
+            status=201,
+        )
