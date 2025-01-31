7f44f455d3f9b5612dcfd226c120a2e2b7b7e3e3
Eduardo Marchi <krzputzdu7@gmail.com>
2025-01-31 14:49:01 -0300

    Adicionado novo app actors

--- genres/admin.py
+++ genres/admin.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
 from genres.models import Genre
+from actors.models import Actor
 
 
 @admin.register(Genre)
 class GenreAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+
+
+@admin.register(Actor)
+class ActorAdmin(admin.ModelAdmin):
     list_display = ('id', 'name')
