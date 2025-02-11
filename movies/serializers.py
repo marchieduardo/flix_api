4b51366e3e020509306ba0ac4dc07cd2fd86c640
Eduardo Marchi <edu.3e@hotmail.com>
2025-02-10 21:52:33 -0300

    Adicionado validações no serializer de movies

--- movies/serializers.py
+++ movies/serializers.py
@@ -7,3 +7,13 @@ class MovieSerializer(serializers.ModelSerializer):
     class Meta:
         model = Movie
         fields = '__all__'
+
+    def validate_resume_date(self, value):
+        if value.year < 1900:
+            raise serializers.ValidationError('A data não pode ser anterior à 1900')
+        return value
+    
+    def validate_resume(self, value):
+        if len(value) > 250:
+            raise serializers.ValidationError('Resumo não pode ter mais de 250 caracteres')
+        return value
