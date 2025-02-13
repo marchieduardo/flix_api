021663001af0aa0706f5c86caa110334c25fe385
Eduardo Marchi <edu.3e@hotmail.com>
2025-02-12 21:50:32 -0300

    Adicionado cálculo de média das reviews no serializer de movies

--- app/urls.py
+++ app/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     
     path('api/v1/', include('genres.urls')),
     path('api/vi/', include('actors.urls')),
-    path('api/v1/', include('movies.views')),
-    path('api/v1/', include('reviews.views')),
+    path('api/v1/', include('movies.urls')),
+    path('api/v1/', include('reviews.urls')),
 ]
--- movies/serializers.py
+++ movies/serializers.py
@@ -1,12 +1,36 @@
 from rest_framework import serializers
+from django.db.models import Avg
 
 from movies.models import Movie
 
 
 class MovieSerializer(serializers.ModelSerializer):
+    rate = serializers.SerializerMethodField(read_only=True)
+
     class Meta:
         model = Movie
         fields = '__all__'
+
+    def get_rate(self,obj):
+        # reviews = obj.reviews.all()    # -----> Método de cálculo da média das reviews escrito na mão
+
+        # if reviews:
+        #     sum_reviews = 0
+
+        #     for review in reviews:
+        #         sum_reviews += review.stars
+
+        #     reviews_count = reviews.count()
+
+        #     return round(sum_reviews / reviews_count, 1)
+        
+        # return None
+
+        rate = obj.reviews.aggregate(Avg('stars'))['stars__avg']     # -----> Função acima reescrita utilizando métodos Aggregate e Avg
+
+        if rate:
+            return round(rate, 1)
+        return None
 
     def validate_resume_date(self, value):
         if value.year < 1900:
