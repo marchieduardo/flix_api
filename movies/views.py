fbe84cab0a0e5dac7bcccc4dd9c375ea74f016fc
Eduardo Marchi <edu.3e@hotmail.com>
2025-02-20 21:41:42 -0300

    Finalizado view do endpoint stats no app movies

--- movies/views.py
+++ movies/views.py
@@ -1,10 +1,12 @@
 # from django.shortcuts import render
 from rest_framework import generics, views, response, status
 from rest_framework.permissions import IsAuthenticated
+from django.db.models import Count, Avg
 
 from movies.models import Movie
 from movies.serializers import MovieSerializer
 from app.permissions import GlobalDefaultPermission
+from reviews.models import Review
 
 
 class MovieCreateListView(generics.ListCreateAPIView):
@@ -19,13 +21,23 @@ class MovieRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = MovieSerializer
 
 
-class MoviewStatsView(views.APIView):
+class MovieStatsView(views.APIView):
     permission_classes = (IsAuthenticated, GlobalDefaultPermission,)
     queryset = Movie.objects.all()
 
 
     def get(self, request):
+        total_movies = self.queryset.count()
+        movies_by_genre = self.queryset.values('genre__name').annotate(count=Count('id'))
+        total_review = Review.objects.count()
+        average_stars = Review.objects.aggregate(avg_stars=Avg('stars'))['avg_stars']
+
         return response.Response(
-            data={'message': 'Funcionou'},
+            data={
+                'total_movies': total_movies,
+                'movies_by_genre': movies_by_genre,
+                'total_review': total_review,
+                'average_stars': round(average_stars, 1) if average_stars else 0,
+            },
             status=status.HTTP_200_OK,
         )
